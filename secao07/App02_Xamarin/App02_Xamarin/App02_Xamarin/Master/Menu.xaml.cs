06afc08fd928e4fbf3f4112891f3126e261ebd63
Guilherme Francisquini <guilherme.santos@itlean.com.br>
2019-07-03 10:47:58 -0300

    Finalização do projeto

--- secao07/App02_Xamarin/App02_Xamarin/App02_Xamarin/Master/Menu.xaml.cs
+++ secao07/App02_Xamarin/App02_Xamarin/App02_Xamarin/Master/Menu.xaml.cs
@@ -10,11 +10,23 @@ using Xamarin.Forms.Xaml;
 namespace App02_Xamarin.Master
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Menu : ContentPage
+    public partial class Menu : MasterDetailPage
     {
         public Menu()
         {
             InitializeComponent();
+        }
+
+        private void GoPageProfile1(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.Perfil1());
+            IsPresented = false;
+        }
+
+        private void GoPageXamarin(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.Xamarin());
+            IsPresented = false;
         }
     }
 }
